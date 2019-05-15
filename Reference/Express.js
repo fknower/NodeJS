570920c6ef95a7c84ec2239b92d349dc3adab778
Mr Francisco Knower <42910041+fknower@users.noreply.github.com>
2019-05-15 16:26:09 +0100

    Update Express.js

--- Reference/Express.js
+++ Reference/Express.js
@@ -11,11 +11,12 @@ app.get('',(req, res) => {
 
                                                     // when "localhost:3000/help" is typed in the browser. It is assigned to req (request)
                                                     // then res (response) 'Hello Express is desplayed in the browser'
+                                                    // this is call a rout
 
 app.get('/help', (req,res) => {
     res.send('Help Page')
 })
-
+                                                    // another example of an About rout
 app.get('/about', (req,res) => {
     res.send('About')
 })
