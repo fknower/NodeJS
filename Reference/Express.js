128a6017e6f83727595229dbacdb611913a0cf7e
Mr Francisco Knower <42910041+fknower@users.noreply.github.com>
2019-05-15 16:46:25 +0100

    Update Express.js

--- Reference/Express.js
+++ Reference/Express.js
@@ -20,12 +20,32 @@ app.get('/help', (req,res) => {
 app.get('/about', (req,res) => {
     res.send('About')
 })
-/
+                                                   //example with weather rout
 app.get('/weather',(req,res) => {
     res.send('Weather')
 })
+                                                   //HTML sent to browser
+app.get('/html',(req,res) => {
+    res.send('<h1>Hello express</h1>')
+})
 
-                                                   //starts the webserver on port 3000
+                                                  // example of JSON being sent to the browser
+app.get('/JSON',(req,res) => {
+    res.send({
+        name: 'Andrew',
+        age: 27
+    })
+})
+                                                // example of a array being sent
+app.get('/JSON',(req,res) => {
+    res.send([{
+        name: 'Andrew',
+        age: 27
+    },{name:'Jane',
+       age: 30
+    }])
+})
+                                                  //starts the webserver on port 3000
 app.listen(3000, () =>{
     console.log('Server is up on port 3000')
 })
